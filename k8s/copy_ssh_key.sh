2651b1cdfb3d0ea952d7db8b9af9a2df7fe89be2
Murthy Karra <karrad@karra.co>
2023-04-16 14:28:23 -0700

    Added update.yaml

--- k8s/copy_ssh_key.sh
+++ k8s/copy_ssh_key.sh
@@ -2,7 +2,7 @@
 echo "vagrant" > password.txt
 for user in vagrant
 do
-  for ip in 10.0.0.200 10.0.0.201 10.0.0.202 10.0.0.203 10.0.0.204 10.0.0.205 10.0.0.206 10.0.0.207
+  for ip in 10.0.0.200 10.0.0.201 10.0.0.202 10.0.0.203 10.0.0.204
   do
     sshpass -f password.txt ssh-copy-id -i ~/.ssh/id_ed25519.pub -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@${ip}
   done
