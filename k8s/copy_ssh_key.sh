a1bb2d3225272d83572807dd3ba62863108e93fb
Murthy Karra <karrad@karra.co>
2023-04-15 23:34:11 -0700

    Initial Commit

---
+++ k8s/copy_ssh_key.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/bash
+echo "vagrant" > password.txt
+for user in vagrant
+do
+  for ip in 10.0.0.200 10.0.0.201 10.0.0.202 10.0.0.203 10.0.0.204 10.0.0.205 10.0.0.206 10.0.0.207
+  do
+    sshpass -f password.txt ssh-copy-id -i ~/.ssh/id_ed25519.pub -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@${ip}
+  done
+done
