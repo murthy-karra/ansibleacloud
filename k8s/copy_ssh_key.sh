e5fc2ef1c0e5674ab93e3021b35d179f43eabbc6
Murthy Karra <karrad@karra.co>
2023-12-02 20:23:07 -0800

    Cleaned up

--- k8s/copy_ssh_key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/bash
-echo "vagrant" > password.txt
-for user in vagrant
-do
-  for ip in 10.0.0.200 10.0.0.201 10.0.0.202 10.0.0.203 10.0.0.204
-  do
-    sshpass -f password.txt ssh-copy-id -i ~/.ssh/id_ed25519.pub -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@${ip}
-  done
-done
